573f7728a2780fbf07c3e7767f844f5c43dc8081
nulli0n <glitchy.cat@ya.ru>
2022-08-25 19:09:13 +0500

    4.0.5

--- src/main/java/su/nightexpress/excellentcrates/Keys.java
+++ src/main/java/su/nightexpress/excellentcrates/Keys.java
@@ -6,6 +6,6 @@ public class Keys {
 
     public static final NamespacedKey CRATE_ID     = new NamespacedKey(ExcellentCratesAPI.PLUGIN, "crate.id");
     public static final NamespacedKey CRATE_KEY_ID = new NamespacedKey(ExcellentCratesAPI.PLUGIN, "crate_key.id");
-    public static final NamespacedKey OLD_CRATES_KEY_ID = new NamespacedKey("goldencrates", "gcrates_key");
+    public static final NamespacedKey OLD_CRATES_KEY_ID = new NamespacedKey(ExcellentCratesAPI.PLUGIN, "gcrates_key");
 
 }
