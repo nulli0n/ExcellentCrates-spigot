42a63382b914693508f0b301448329ceadbf268f
BuildTools <unconfigured@null.spigotmc.org>
2024-04-17 02:14:46 +0500

    v5.2.2

--- src/main/java/su/nightexpress/excellentcrates/crate/impl/Reward.java
+++ src/main/java/su/nightexpress/excellentcrates/crate/impl/Reward.java
@@ -4,7 +4,6 @@ import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import su.nightexpress.excellentcrates.CratesPlugin;
