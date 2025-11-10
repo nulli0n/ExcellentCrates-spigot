c852b0e13c7170fe92e3c3a9cdd7b6e7bed89115
nulli0n <glitchy.cat@yandex.ru>
2025-11-10 16:38:06 +0500

    Revert deep folder inspection

--- src/main/java/su/nightexpress/excellentcrates/crate/CrateManager.java
+++ src/main/java/su/nightexpress/excellentcrates/crate/CrateManager.java
@@ -175,7 +175,7 @@ public class CrateManager extends AbstractManager<CratesPlugin> {
     }
 
     private void loadCrates() {
-        for (File file : FileUtil.getFiles(plugin.getDataFolder() + Config.DIR_CRATES, true)) {
+        for (File file : FileUtil.getFiles(plugin.getDataFolder() + Config.DIR_CRATES, false)) {
             String id = Strings.varStyle(FileConfig.getName(file)).orElseThrow(); // TODO Handle
 
             Crate crate = new Crate(plugin, file.toPath(), id);
